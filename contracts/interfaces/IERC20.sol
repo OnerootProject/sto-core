db3f5e54f7b1c039ae9235a71ab6e7139757625e
tercel <>
2020-07-29 19:38:55 +0800

    import the project draft code

---
+++ contracts/interfaces/IERC20.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @title Interface of ERC20 standard
+ * @dev ref. https://eips.ethereum.org/EIPS/eip-777
+ */
+
+contract IERC20 {
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
