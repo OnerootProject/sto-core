cd2c2c39c2d0258074b256f7e7795fc7da10f117
tercel <>
2020-07-31 21:28:47 +0800

    init doc

---
+++ doc/public/js/config/contract.dev.js
@@ -0,0 +1,10 @@
+var contractDev = {
+        RAC: '0xcbf9ae3ef7e17bbbe7b9f997505c01c842d88673',
+        PolicyRegistry: '0x2ad7e42a43661f808a00d3953a4970d6732d4c32',
+        STGFactory: '0x64939649b2a47c5e889464b35e640fd53cb1ebda',
+        STOFactory: '0x92ca5baa9849a569c5f445b7f226381f362febac',
+        ST: '0x88d4c86eb79384c4dcd486f0fdc82692cc463a42',
+        GP: '0x30f22a7fe362ba089409815cc5eb65422b23a47c',
+        STO: '0x6c0f123faeb77090e28225976e597ca865ee713e'
+};
+    
